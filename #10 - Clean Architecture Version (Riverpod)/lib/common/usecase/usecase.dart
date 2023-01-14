7d9a587c6d8240d7116b357a1a67fb6dcd3ec4c6
SinaSys <sinasystem47@gmail.com>
2023-01-14 12:47:55 -0800

    Remove riverpod version

--- #10 - Clean Architecture Version (Riverpod)/lib/common/usecase/usecase.dart
+++
@@ -1,6 +0,0 @@
-
-import '../network/api_result.dart';
-
-abstract class UseCase<Type, Params> {
-  Future<ApiResult<Type>> call(Params params);
-}
--- #10 - Clean Architecture Version (Riverpod)/lib/core/app/app_asset.dart
+++
@@ -1,8 +0,0 @@
-class AppAsset {
-  const AppAsset._();
-
-  static const male = "asset/images/male.png";
-  static const female = "asset/images/female.png";
-  static const user = "asset/images/user.png";
-  static const emptyState = "asset/images/empty_state.png";
-}
