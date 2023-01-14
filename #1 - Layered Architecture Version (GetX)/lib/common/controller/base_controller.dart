10e39fcf9c4e6c1046674a9c5cc31ec6609d8bae
SinaSys <sinasystem47@gmail.com>
2023-01-14 09:27:05 -0800

    Reduce code duplication using template method

--- #1 - Layered Architecture Version (GetX)/lib/common/controller/base_controller.dart
+++ #1 - Layered Architecture Version (GetX)/lib/common/controller/base_controller.dart
@@ -1,7 +1,7 @@
-import 'package:dartz/dartz.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:dartz/dartz.dart';
 
- enum ApiState { loading, failure, success }
+enum ApiState { loading, failure, success }
 
 mixin BaseController {
   Rx<ApiState> apiStatus = ApiState.loading.obs;
@@ -9,31 +9,31 @@ mixin BaseController {
 
   _checkFailureOrSuccess(Either<String, bool> failureOrSuccess) {
     failureOrSuccess.fold(
-          (String failure) {
+      (String failure) {
         apiStatus.value = ApiState.failure;
         errorMessage = failure.obs;
       },
-          (bool success) {
+      (bool success) {
         apiStatus.value = ApiState.success;
       },
     );
   }
 
-  void createItem(Future<Either<String, bool>> apiCallback) async {
+  _apiOperationTemplate(Future<Either<String, bool>> apiCallback) async {
     apiStatus.value = ApiState.loading;
     Either<String, bool> failureOrSuccess = await apiCallback;
     _checkFailureOrSuccess(failureOrSuccess);
+  }
+
+  void createItem(Future<Either<String, bool>> apiCallback) async {
+    _apiOperationTemplate(apiCallback);
   }
 
   void updateItem(Future<Either<String, bool>> apiCallback) async {
-    apiStatus.value = ApiState.loading;
-    Either<String, bool> failureOrSuccess = await apiCallback;
-    _checkFailureOrSuccess(failureOrSuccess);
+    _apiOperationTemplate(apiCallback);
   }
 
   void deleteItem(Future<Either<String, bool>> apiCallback) async {
-    apiStatus.value = ApiState.loading;
-    Either<String, bool> failureOrSuccess = await apiCallback;
-    _checkFailureOrSuccess(failureOrSuccess);
+    _apiOperationTemplate(apiCallback);
   }
 }
