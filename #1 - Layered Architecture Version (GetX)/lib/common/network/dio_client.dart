8f4b16bb9ce74933ca7fbbe688567760bfb1ff7f
sina <sinasystem47@gmail.com>
2023-02-23 01:08:28 +0330

    L.A version (Getx) : Migrate Dio package from v4 to v5

--- #1 - Layered Architecture Version (GetX)/lib/common/network/dio_client.dart
+++ #1 - Layered Architecture Version (GetX)/lib/common/network/dio_client.dart
@@ -2,6 +2,7 @@ import 'package:layered_architecture/common/network/dio_interceptor.dart';
 import 'package:layered_architecture/core/api_config.dart';
 import 'package:dio/dio.dart';
 
+//singleton class for DioClient
 class DioClient {
   static final DioClient _instance = DioClient._internal();
   late final Dio? dio;
@@ -11,11 +12,12 @@ class DioClient {
   DioClient._internal() {
     dio = Dio(
       BaseOptions(
-          baseUrl: ApiConfig.baseUrl,
-          headers: ApiConfig.header,
-          connectTimeout: ApiConfig.connectionTimeout,
-          receiveTimeout: ApiConfig.receiveTimeout,
-          responseType: ResponseType.json),
+        baseUrl: ApiConfig.baseUrl,
+        headers: ApiConfig.header,
+        connectTimeout: ApiConfig.connectionTimeout,
+        receiveTimeout: ApiConfig.receiveTimeout,
+        responseType: ResponseType.json,
+      ),
     )..interceptors.add(DioInterceptor());
   }
 }
