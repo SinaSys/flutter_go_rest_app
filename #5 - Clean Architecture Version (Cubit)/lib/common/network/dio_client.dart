a48b81f74c4985207151276ea25e3f1044a4c660
sina <sinasystem47@gmail.com>
2023-02-23 01:27:32 +0330

    C.A version Cubit : Migrate Dio package from v4 to v5

--- #5 - Clean Architecture Version (Cubit)/lib/common/network/dio_client.dart
+++ #5 - Clean Architecture Version (Cubit)/lib/common/network/dio_client.dart
@@ -1,6 +1,6 @@
 import 'package:clean_architecture_cubit/common/network/api_config.dart';
 import 'package:clean_architecture_cubit/common/network/dio_interceptor.dart';
-import 'package:dio/dio.dart';
+import 'package:dio/dio.dart' show Dio, ResponseType;
 
 class DioClient {
   final Dio dio;
