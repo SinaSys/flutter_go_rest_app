04b65b105f8a3045b2995409ea4bdbe2cfc42530
SINASYS <sinasystem47@gmail.com>
2023-05-20 15:00:35 +0430

    C.A version (GetX) : Make entity classes immutable

--- #6 - Clean Architecture Version (Getx)/lib/features/post/data/models/post.dart
+++ #6 - Clean Architecture Version (Getx)/lib/features/post/data/models/post.dart
@@ -1,6 +1,6 @@
-import 'package:clean_architecture_getx/features/post/domain/entities/post_entity.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:flutter/foundation.dart' show immutable;
+import 'package:clean_architecture_getx/features/post/domain/entities/post_entity.dart';
 
 part 'post.g.dart';
 
--- #6 - Clean Architecture Version (Getx)/lib/features/todo/data/models/todo.dart
+++ #6 - Clean Architecture Version (Getx)/lib/features/todo/data/models/todo.dart
@@ -1,6 +1,6 @@
-import 'package:clean_architecture_getx/features/todo/domain/entities/todo_entity.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:flutter/foundation.dart' show immutable;
+import 'package:clean_architecture_getx/features/todo/domain/entities/todo_entity.dart';
 
 part 'todo.g.dart';
 
