9166afa1719a8df21b9930248127aa1bf2373664
SINASYS <sinasystem47@gmail.com>
2023-11-30 18:12:13 +0330

    C.A version (GetX) : Change logic of dioClient class for better unit testing

--- #6 - Clean Architecture Version (Getx)/lib/common/network/dio_client.dart
+++ #6 - Clean Architecture Version (Getx)/lib/common/network/dio_client.dart
@@ -1,11 +1,15 @@
+import 'package:dio/dio.dart';
 import 'package:clean_architecture_getx/common/network/api_config.dart';
 import 'package:clean_architecture_getx/common/network/dio_interceptor.dart';
-import 'package:dio/dio.dart' show Dio, ResponseType;
 
+/// DioClient is a wrapper class around dio package. this class can hide complexity of
+/// dio and its accessibility from outside world and also make dio testable for our
+/// remote data source classes
 class DioClient {
-  final Dio dio;
+  final Dio _dio;
 
-  DioClient(this.dio) {
+  //Add global configuration for all APIs
+  DioClient(Dio dio) : _dio = dio {
     dio
       ..options.baseUrl = ApiConfig.baseUrl
       ..options.headers = ApiConfig.header
@@ -13,5 +17,60 @@ class DioClient {
       ..options.receiveTimeout = ApiConfig.receiveTimeout
       ..options.responseType = ResponseType.json
       ..interceptors.add(DioInterceptor());
+  }
+
+  Future<Response<T>> get<T>(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    return await _dio.get(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+    );
+  }
+
+  Future<Response<T>> post<T>(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    return await _dio.post(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+
+  Future<Response<T>> put<T>(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    return await _dio.put(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+
+  Future<Response<T>> delete<T>(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    return await _dio.delete(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
   }
 }
