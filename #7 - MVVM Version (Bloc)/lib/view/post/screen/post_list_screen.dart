b7cf526160ddb5081720d1acc6ff290e18664d37
SINASYS <sinasystem47@gmail.com>
2023-12-05 20:38:59 +0330

    MVVM version (Bloc) : Fix bug : overflow problem on post list screen

--- #7 - MVVM Version (Bloc)/lib/view/post/screen/post_list_screen.dart
+++ #7 - MVVM Version (Bloc)/lib/view/post/screen/post_list_screen.dart
@@ -1,17 +1,17 @@
-import 'package:mvvm_bloc/view/post/screen/create_post_screen.dart';
-import 'package:mvvm_bloc/common/widget/spinkit_indicator.dart';
-import 'package:mvvm_bloc/common/bloc/generic_bloc_state.dart';
+import 'package:flutter/material.dart';
+import 'package:mvvm_bloc/core/app_style.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mvvm_bloc/core/app_extension.dart';
+import 'package:mvvm_bloc/data/model/user/user.dart';
+import 'package:mvvm_bloc/data/model/post/post.dart';
 import 'package:mvvm_bloc/common/dialog/retry_dialog.dart';
 import 'package:mvvm_bloc/common/widget/empty_widget.dart';
-import 'package:mvvm_bloc/core/app_extension.dart';
-import 'package:mvvm_bloc/core/app_style.dart';
-import 'package:mvvm_bloc/data/model/post/post.dart';
-import 'package:mvvm_bloc/data/model/user/user.dart';
-import 'package:mvvm_bloc/view/post/screen/post_detail_screen.dart';
 import 'package:mvvm_bloc/viewmodel/post/bloc/post_bloc.dart';
+import 'package:mvvm_bloc/common/bloc/generic_bloc_state.dart';
 import 'package:mvvm_bloc/viewmodel/post/bloc/post_event.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter/material.dart';
+import 'package:mvvm_bloc/common/widget/spinkit_indicator.dart';
+import 'package:mvvm_bloc/view/post/screen/create_post_screen.dart';
+import 'package:mvvm_bloc/view/post/screen/post_detail_screen.dart';
 
 class PostListScreen extends StatefulWidget {
   const PostListScreen({
@@ -90,7 +90,9 @@ class _PostListScreenState extends State<PostListScreen> {
                     return Text(
                       postBloc.getPostCount,
                       style: const TextStyle(
-                          color: Colors.black54, fontWeight: FontWeight.w500),
+                        color: Colors.black54,
+                        fontWeight: FontWeight.w500,
+                      ),
                     );
                   },
                 )
@@ -104,62 +106,59 @@ class _PostListScreenState extends State<PostListScreen> {
   }
 
   Widget userPostItem(List<Post> posts) {
-    return Expanded(
-      child: ListView.builder(
-        shrinkWrap: true,
-        itemCount: posts.length,
-        itemBuilder: (_, index) {
-          Post post = posts[index];
-          return Center(
-            child: GestureDetector(
-              onTap: () async {
-                var resultFromPostDetailScreen = await Navigator.push(
-                  _,
-                  MaterialPageRoute(
-                    builder: (_) {
-                      return PostDetailScreen(post: post, user: widget.user);
-                    },
-                  ),
-                );
-
-                if (resultFromPostDetailScreen != null &&
-                    resultFromPostDetailScreen) {
-                  getData();
-                }
-              },
-              child: Card(
-                margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: posts.length,
+      itemBuilder: (_, index) {
+        Post post = posts[index];
+        return Center(
+          child: GestureDetector(
+            onTap: () async {
+              var resultFromPostDetailScreen = await Navigator.push(
+                _,
+                MaterialPageRoute(
+                  builder: (_) {
+                    return PostDetailScreen(post: post, user: widget.user);
+                  },
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(post.title, maxLines: 2, style: headLine2),
-                            const SizedBox(height: 10),
-                            Text(
-                              post.body,
-                              maxLines: 3,
-                              overflow: TextOverflow.ellipsis,
-                            )
-                          ],
-                        ),
+              );
+
+              if (resultFromPostDetailScreen != null && resultFromPostDetailScreen) {
+                getData();
+              }
+            },
+            child: Card(
+              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(post.title, maxLines: 2, style: headLine2),
+                          const SizedBox(height: 10),
+                          Text(
+                            post.body,
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                          )
+                        ],
                       ),
-                      const Icon(Icons.arrow_forward_ios_sharp)
-                    ],
-                  ),
+                    ),
+                    const Icon(Icons.arrow_forward_ios_sharp)
+                  ],
                 ),
               ),
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 
@@ -168,8 +167,7 @@ class _PostListScreenState extends State<PostListScreen> {
     return Scaffold(
       floatingActionButton: floatingActionButton(context),
       appBar: appBar,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: ListView(
         children: [
           header,
           const Padding(
