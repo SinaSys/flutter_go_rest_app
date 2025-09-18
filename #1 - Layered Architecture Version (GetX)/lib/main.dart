81559afc3c332b492e5acb3886bd930f97723546
SINASYS <sinasystem47@gmail.com>
2025-09-19 01:40:33 +0430

    L.A Version (GetX): Support dynamic backend selection at startup

--- #1 - Layered Architecture Version (GetX)/lib/main.dart
+++ #1 - Layered Architecture Version (GetX)/lib/main.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:layered_architecture/core/app_theme.dart';
+import 'package:layered_architecture/core/app_config.dart';
+import 'package:layered_architecture/core/app_string.dart';
+import 'package:layered_architecture/core/app_style.dart' show logger;
 import 'package:layered_architecture/features/user/view/screen/user_list_screen.dart';
 
-void main() => runApp(const MyApp());
+
+Future<void> main() async {
+  await initApp();
+}
+
+Future<void> initApp() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  const backendStr = String.fromEnvironment(
+    'BACKEND',
+    defaultValue: AppString.gorestEnv,
+  );
+  final backend = BackendEnv.fromString(backendStr);
+
+  await ConfigLoader.load(backend);
+
+  logger.i('Running with backend: ${ConfigLoader.currentEnv}');
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
