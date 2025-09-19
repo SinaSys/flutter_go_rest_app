3aaa204b06777d897de84e26af28d0e36292c6d5
SINASYS <sinasystem47@gmail.com>
2025-09-19 13:55:41 +0430

    L.A Version (Bloc) : Load baseUrl dynamically from AppConfig

--- #3 - Layered Architecture Version (Bloc)/lib/core/api_config.dart
+++ #3 - Layered Architecture Version (Bloc)/lib/core/api_config.dart
@@ -1,10 +1,10 @@
+import 'package:layered_architecture_bloc/core/app_config.dart';
 import 'package:layered_architecture_bloc/core/app_config.dart' show ConfigLoader;
 
 class ApiConfig {
   ApiConfig._();
 
-  static const token = "6369de3d8da8e764cc3c1cbe4121bb6741875de2c30dfd39df8fd351b6a508ea";
-  static const String baseUrl = "https://gorest.co.in/public/v2";
+  static final String baseUrl = ConfigLoader.instance.backendUrl;
   static const Duration receiveTimeout = Duration(milliseconds: 15000);
   static const Duration connectionTimeout = Duration(milliseconds: 15000);
   static const String users = '/users';
