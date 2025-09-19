e3d8f02f11de6e6552937c1710a955c1d31ce423
SINASYS <sinasystem47@gmail.com>
2025-09-19 14:05:44 +0430

    C.A Version (Bloc) : Add asset paths for backend JSON configs

--- #4 - Clean Architecture Version (Bloc)/lib/core/app_asset.dart
+++ #4 - Clean Architecture Version (Bloc)/lib/core/app_asset.dart
@@ -5,4 +5,8 @@ class AppAsset {
   static const female = "asset/images/female.png";
   static const user = "asset/images/user.png";
   static const emptyState = "asset/images/empty_state.png";
+
+  // Configs
+  static const gorestConfig = "asset/config/gorest_config.json";
+  static const springConfig = "asset/config/spring_config.json";
 }
