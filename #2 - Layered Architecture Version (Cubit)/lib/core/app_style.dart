bf26ce8e28343b524a2cd2c6cc8c85751eda783d
SINASYS <sinasystem47@gmail.com>
2025-09-19 12:30:34 +0430

    L.A Version (Cubit) : Move logger to app_style file

--- #2 - Layered Architecture Version (Cubit)/lib/core/app_style.dart
+++ #2 - Layered Architecture Version (Cubit)/lib/core/app_style.dart
@@ -1,4 +1,11 @@
+import 'package:logger/logger.dart';
 import 'package:flutter/material.dart';
+
+final Logger logger = Logger(
+  printer: PrettyPrinter(
+    methodCount: 0,
+  ),
+);
 
 const List<Color> colorList = [
   Color(0xFFF4511E),
