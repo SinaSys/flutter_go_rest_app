8ae66e8217d95a99e0fcae0e2452142839296323
SINASYS <sinasystem47@gmail.com>
2025-09-19 14:05:10 +0430

    C.A Version (Bloc) : Move logger to app_style file

--- #4 - Clean Architecture Version (Bloc)/lib/common/network/dio_interceptor.dart
+++ #4 - Clean Architecture Version (Bloc)/lib/common/network/dio_interceptor.dart
@@ -1,15 +1,8 @@
 import 'package:dio/dio.dart';
-import 'package:logger/logger.dart';
 import 'package:clean_architecture_bloc/core/app_extension.dart';
+import 'package:clean_architecture_bloc/core/app_style.dart' show logger;
 
 class DioInterceptor extends Interceptor {
-  final Logger logger = Logger(
-    printer: PrettyPrinter(
-      methodCount: 0,
-      printTime: false,
-    ),
-  );
-
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     logger.i('====================START====================');
@@ -22,15 +15,15 @@ class DioInterceptor extends Interceptor {
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) {
     final options = err.requestOptions;
-    logger.e(options.method); // Debug log
-    logger.e('Error: ${err.error}, Message: ${err.message}'); // Error log
+    logger.e(options.method);
+    logger.e('Error: ${err.error}, Message: ${err.message}');
     return super.onError(err, handler);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    logger.d('Response => StatusCode: ${response.statusCode}'); // Debug log
-    logger.d('Response => Body: ${response.data}'); // Debug log
+    logger.i('Response => StatusCode: ${response.statusCode}');
+    logger.i('Response => Body: ${response.data}');
     return super.onResponse(response, handler);
   }
 }
