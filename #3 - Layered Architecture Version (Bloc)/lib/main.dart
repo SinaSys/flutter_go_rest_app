7c75999059f3b980fdaf038a43c1f04dd607cdd1
SINASYS <sinasystem47@gmail.com>
2025-09-19 13:17:05 +0430

    L.A Version (Bloc) : Support dynamic backend selection at startup

--- #3 - Layered Architecture Version (Bloc)/lib/main.dart
+++ #3 - Layered Architecture Version (Bloc)/lib/main.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:layered_architecture_bloc/core/app_theme.dart';
+import 'package:layered_architecture_bloc/core/app_style.dart' show logger;
 import 'package:layered_architecture_bloc/features/post/bloc/post_bloc.dart';
 import 'package:layered_architecture_bloc/features/todo/bloc/todo_bloc.dart';
 import 'package:layered_architecture_bloc/features/user/bloc/user_bloc.dart';
+import 'package:layered_architecture_bloc/core/app_string.dart' show AppString;
 import 'package:layered_architecture_bloc/features/comment/bloc/comment_bloc.dart';
 import 'package:layered_architecture_bloc/features/user/view/screen/user_list_screen.dart';
+import 'package:layered_architecture_bloc/core/app_config.dart' show BackendEnv, ConfigLoader;
 
-void main() => runApp(const MyApp());
+Future<void> main() async => await initApp();
+
+Future<void> initApp() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  const backendStr = String.fromEnvironment(
+    'BACKEND',
+    defaultValue: AppString.gorestEnv,
+  );
+  final backend = BackendEnv.fromString(backendStr);
+
+  await ConfigLoader.load(backend);
+
+  logger.i('Running with backend: ${ConfigLoader.currentEnv}');
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
